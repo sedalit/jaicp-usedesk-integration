f78b766cfd26809cf19b4134c3f495968c9982eb
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-01-27 19:04:51 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 require 'vendor/autoload.php';
 
+use Sedalit\JaicpUsedeskIntegration\Core\Integration;
 use Sedalit\JaicpUsedeskIntegration\Http\Request;
 
 $request = new Request();
+$integration = new Integration();
 
+return $integration->handleRequest();
