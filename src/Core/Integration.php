6fe83f6427617d943f2190404cc14432a6f2899e
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-01-28 18:02:38 +1000

    Update Integration.php

--- src/Core/Integration.php
+++ src/Core/Integration.php
@@ -1,7 +1,24 @@
 <?php
 
 namespace Sedalit\JaicpUsedeskIntegration\Core;
+use Sedalit\JaicpUsedeskIntegration\Core\Handlers\Handler;
+use Sedalit\JaicpUsedeskIntegration\Usedesk\Client;
+use Sedalit\JaicpUsedeskIntegration\Usedesk\Ticket;
 
 class Integration {
-    
+    protected $ticket;
+    protected $usedeskClient;
+    protected $handler;
+
+    function __construct() {
+        $request = request();
+
+        $this->usedeskClient = new Client($request->client());
+        $this->ticket = new Ticket($request->ticket());
+        $this->handler = new Handler($this->usedeskClient, $this->ticket);
+    }
+
+    public function handle() {
+        $this->handler->handle();
+    }
 }
