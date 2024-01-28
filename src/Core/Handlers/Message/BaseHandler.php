13a8cc5c360406369c93f77cbfee2d3b55aa668c
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-01-28 18:25:10 +1000

    Update BaseHandler.php

--- src/Core/Handlers/Message/BaseHandler.php
+++ src/Core/Handlers/Message/BaseHandler.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace Sedalit\JaicpUsedeskIntegration\Core\Handlers\Message;
+use Sedalit\JaicpUsedeskIntegration\Jaicp\JaicpInterface;
+use Sedalit\JaicpUsedeskIntegration\Usedesk\UsedeskInterface;
 
 abstract class BaseHandler {
     protected $usedeskInterface;
     protected $jaicpInterface;
+    protected $ticket;
+    protected $usedeskClient;
 
-    function __construct($usedeskInterface, $jaicpInterface) {
-        $this->usedeskInterface = $usedeskInterface;
-        $this->jaicpInterface = $jaicpInterface;
+    protected function boot($ticket, $usedeskClient) {
+        $this->ticket = $ticket;
+        $this->usedeskClient = $usedeskClient;
+
+        $this->usedeskInterface = new UsedeskInterface($ticket->assigneeId, request()->chatId(), $ticket->id, request()->platform(), $ticket->channelId);
+        $this->jaicpInterface = new JaicpInterface($ticket->id, $ticket->message, []);
     }
 }
