02368e98442acfb88dc266d2c027139f75528c64
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-02-18 18:48:26 +1000

    Update MessageFromClient.php

--- src/Core/Handlers/Message/MessageFromClient.php
+++ src/Core/Handlers/Message/MessageFromClient.php
@@ -6,19 +6,14 @@ use Sedalit\JaicpUsedeskIntegration\Core\Handlers\IRequestHandler;
 
 class MessageFromClient extends BaseHandler implements IRequestHandler {
     function canHandle() {
-        $isBotAssigned = $this->ticket->assigneeId == env()->usedesk('botUserId');
-        $isOperatorGroupAssigned = $this->isOperatorGroupAssigned();
-        return (request()->chatId() != false && request()->json()['from'] == "client") && ($isBotAssigned == true && $isOperatorGroupAssigned != true);
+        $isMessageToBot = $this->ticket->assigneeId() == env()->usedesk('botUserId') || (!$this->ticket->assigneeId() && !$this->ticket->groupId());
+        $isMessageFromClient = request()->json()['from'] == "client";
+        return $isMessageToBot && $isMessageFromClient;
     }
 
     function handleRequest($request) {
         $answerFromBot = $this->jaicpInterface->sendMessage();
         $usedeskRequestResult = $this->usedeskInterface->sendMessage($answerFromBot);
-
         return compact($answerFromBot, $usedeskRequestResult);
-    }
-
-    protected function isOperatorGroupAssigned() {
-        return $this->ticket->group == env()->operatorGroups('defaultOperatorGroupID') || $this->ticket->group == env()->operatorGroups($this->ticket->channel());
     }
 }
