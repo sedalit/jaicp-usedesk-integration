6ec3f0a6f56b930b13c10a03796e6f4294663eaf
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-02-18 18:48:36 +1000

    Update JaicpApiRequest.php

--- src/Jaicp/JaicpApiRequest.php
+++ src/Jaicp/JaicpApiRequest.php
@@ -5,24 +5,32 @@ namespace Sedalit\JaicpUsedeskIntegration\Jaicp;
 use Sedalit\JaicpUsedeskIntegration\Core\ApiRequest;
 
 class JaicpApiRequest extends ApiRequest {
+    protected const PROTOCOL = "https://";
     protected $requestData;
 
-    function __construct($url, $settings = [], $requestData){
-        $this->url = $url;
-        $this->settings = $settings;
+    function __construct($url, $requestData, $settings = []){
+        $this->url = self::PROTOCOL . $url;
         $this->requestData = $requestData;
+        $this->setSettings($settings);
+    }
+
+    protected function setSettings($settings) {
+        $this->settings = $settings + $this->defaultSettings();
     }
 
     protected function getSettings(){
+        return $this->settings;
+    }
+
+    protected function defaultSettings() {
         return [
             CURLOPT_URL => $this->url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_HTTPHEADER => array('Content-Type:application/json'),
             CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => $this->requestData->json()
+            CURLOPT_POSTFIELDS => $this->requestData->get()
         ];
     }
-
 
 }
