3af3ebc274b76b48a32ab201f46a5017c419cba5
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-02-24 16:28:45 +1000

    Update UsedeskApiRequest.php

--- src/Usedesk/UsedeskApiRequest.php
+++ src/Usedesk/UsedeskApiRequest.php
@@ -4,28 +4,35 @@ namespace Sedalit\JaicpUsedeskIntegration\Usedesk;
 use Sedalit\JaicpUsedeskIntegration\Core\ApiRequest;
 
 class UsedeskApiRequest extends ApiRequest {
+
+    /**
+     * @var UsedeskApiRequestData Объект, хранящий данные для отправки в Usedesk
+     */
     protected $requestData;
 
-    function __construct($url, $requestData, $settings = []){
+    function __construct($url, $requestData, $settings = [])
+    {
         $this->url = UsedeskInterface::USEDESK_HOST . $url;
         $this->requestData = $requestData;
-        $this->settings = $this->setSettings($settings);
+        $this->setSettings($settings);
     }
 
-    public function getSettings() {
+    public function getSettings() : array
+    {
         return $this->settings;
     }
 
-    protected function setSettings($settings) {
+    protected function setSettings($settings) : void
+    {
         $data = [
             'api_token' => env()->tokens('usedeskApiToken'),
         ];
 
         $requestData = $this->requestData->get();
 
-        $data = $data + $requestData[0];
+        $data = $data + $requestData;
 
-        return [
+        $this->settings = [
             CURLOPT_URL => $this->url,
             CURLOPT_USERAGENT => 'PHP-MCAPI/2.0',
             CURLOPT_RETURNTRANSFER => true,
