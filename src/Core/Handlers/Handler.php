60c44279f2758d3cad27a3145125b0e9c253ed68
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2024-02-24 16:29:36 +1000

    Update Handler.php

--- src/Core/Handlers/Handler.php
+++ src/Core/Handlers/Handler.php
@@ -2,60 +2,73 @@
 
 namespace Sedalit\JaicpUsedeskIntegration\Core\Handlers;
 
+use Sedalit\JaicpUsedeskIntegration\Usedesk\Client;
+use Sedalit\JaicpUsedeskIntegration\Usedesk\Ticket;
+
 class Handler {
+    /**
+     * @var Client Объект текущего клиента в Usedesk
+    */
     protected $usedeskClient;
+
+    /**
+     * @var Ticket Объект текущего запроса (тикета) в Usedesk
+    */
     protected $ticket;
+
+    /**
+     * @var array Массив инициализированных обработчиков
+    */
     protected $handlers = [];
+
+    /**
+     * @var HandlerBoot Объект инициализатора обработчиков
+    */
     protected $handlerBooter;
 
-    function __construct($usedeskClient, $ticket) {
+    function __construct(Client $usedeskClient, Ticket $ticket) 
+    {
         $this->usedeskClient = $usedeskClient;
         $this->ticket = $ticket;
         $this->handlerBooter = new HandlerBoot();
     }
 
-    public function handle() {
-        $this->initHandlers();
+    /**
+     * Функция, вызывающая все подходящие обработчики для текущего запроса
+     * @return array Массив с результатами обработки запроса
+    */
+    public function handle() : array
+    {
+        $requestType = request()->chatId() ? "message" : "trigger";
+        $this->initHandlers($requestType);
 
-        $messagesResult = $this->handleMessage();
-        $triggersResult = $this->handleTrigger();
-
-        return ['messages' => $messagesResult, 'triggers' => $triggersResult];
+        return $this->handleTypedRequest();
     }
 
-    protected function handleMessage() {
-        if (isset($this->handlers['message'])){
-            $result = [];
-            foreach ($this->handlers['message'] as $handler) {
-                if (call_user_func_array([$handler, 'canHandle'], [])) {
-                    $result[$handler] = call_user_func_array([$handler, 'handleRequest'], [request()]);
-                } else {
-                    $result[$handler] = "Can't handle";
-                }
+    /**
+     * Функция, проверяющая условия запуска обработчика и запускающая его для обработки
+     * @return array Результаты работы обработчиков
+    */
+    protected function handleTypedRequest() : array
+    {
+        $result = [];
+        foreach ($this->handlers as $handler) {
+            if (call_user_func_array([$handler, 'canHandle'], [])) {
+                $result[$handler] = call_user_func_array([$handler, 'handleRequest'], [request()]);
+            } else {
+                $result[$handler] = "Can't handle";
             }
- 
-            return $result;
         }
-        return "No handlers booted";
+        var_dump($result);
+        return $result;
     }
 
-    protected function handleTrigger() {
-        if (isset($this->handlers['trigger'])){
-            $result = [];
-            foreach ($this->handlers['trigger'] as $handler) {
-                if (call_user_func_array([$handler, 'canHandle'], [])) {
-                    $result[$handler] = call_user_func_array([$handler, 'handleRequest'], [request()]);
-                } else {
-                    $result[$handler] = "Can't handle";
-                }
-            }
-
-            return $result;
-        }
-        return "No handlers booted";
-    }
-
-    protected function initHandlers() {
-        $this->handlers = $this->handlerBooter->boot($this->ticket, $this->usedeskClient);
+    /**
+     * Функция, инициализирующая все обработчики для текущего типа запроса
+     * @param string $requestType Тип запроса (сообщение или триггер)
+    */
+    protected function initHandlers(string $requestType) : void
+    {
+        $this->handlers = $this->handlerBooter->boot($this->ticket, $this->usedeskClient, $requestType);
     }
 }
